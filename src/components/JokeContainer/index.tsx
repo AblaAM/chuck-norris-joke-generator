03ab6605d9f0ccf55f1666cca03291211320c29f
AblaAM <ablaammami@think-it.io>
2021-10-05 19:29:32 +0100

    feat: Add `main` and `joke` containers

--- src/components/JokeContainer/index.tsx
+++ src/components/JokeContainer/index.tsx
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
+import img from "../../assets/chucknorris_logo_coloured_small@2x.png";
+import chuckNorris from "../../assets/chuck-norris.png";
 
-const JokeContainer: React.FC = () => {
+import JokeModel from "../../models/jokeModel";
+interface HttpResponse<T> extends Response {
+  parsedBody?: T;
+}
+const JokeContainer: React.FC<{ category: string }> = ({ category }) => {
+  const [data, dataSet] = useState<JokeModel>();
+  const [loading, loadingSet] = useState<boolean>(false);
+  const [error, errorSet] = useState<boolean>(false);
+
+  async function fetchJoke<T>(request: RequestInfo): Promise<HttpResponse<T>> {
+    loadingSet(true);
+    const response: HttpResponse<T> = await fetch(request);
+    response.parsedBody = await response.json();
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    loadingSet(false);
+    return response;
+  }
+
+  const clickButton = async () => {
+    let response: HttpResponse<JokeModel>;
+    try {
+      response = await fetchJoke<JokeModel>(
+        category === "all"
+          ? "https://api.chucknorris.io/jokes/random"
+          : `https://api.chucknorris.io/jokes/random/?category=${category}`
+      );
+      dataSet(response.parsedBody);
+      errorSet(false);
+    } catch (response) {
+      console.log(response);
+      errorSet(true);
+    }
+  };
   return (
     <>
-      <p className="logo"></p>
+      <div className="main-container">
+        <div className="joke-container">
+          <button className="joke-button" onClick={clickButton}>
+            Click here to generate a joke
+          </button>
+          <div className="image-container">
+            <img src={img} className="chuck-image" />
+          </div>
+        </div>
+        <div className="container">
+          {!loading && data && !error ? (
+            <div className="joke-body">
+              <h1 className="categorie">
+                {data.categories ? data.categories[0] : null}
+              </h1>
+              <img src={data.icon_url}></img>
+              <h1 className="joke">{data.value}</h1>
+            </div>
+          ) : loading && !error ? (
+            <div className="loader"></div>
+          ) : error ? (
+            <p>PLEASE TRY AGAIN</p>
+          ) : (
+            <img src={chuckNorris} className="image-placeholder" />
+          )}
+        </div>
+      </div>
     </>
   );
 };
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import JokeContainer from "./JokeContainer";
 import Header from "./Header";
+import Model from "./Model";
 import Footer from "./Footer";
 
 const Main: React.FC = () => {
+  const [data, setData] = useState<Array<string>>([]);
+  const [category, setCategory] = useState<string>("all");
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/categories")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.log("error:", error);
+      });
+  }, []);
+
+  const handleSelectChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
+    setCategory(event.target.value);
+  };
   return (
-    <>
+    <div className="app-container">
       <Header />
-      <div className="title-container">
-        <p className="title">Chuck norris Joke Generator</p>
+      <div className="main-heading">
+        <div className="heading"></div>
+        <div className="select-container">
+          <select onChange={handleSelectChange} className="select">
+            <option key="0" value="all">
+              all
+            </option>
+            {data.length &&
+              data.map((item: string, index: number) => {
+                return (
+                  <option key={`${index}+1`} value={item}>
+                    {item}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
       </div>
-      <JokeContainer />
+      <JokeContainer category={category} />
+      <Model />
       <Footer />
-    </>
+    </div>
   );
 };
 
