6aae24608e92eef1b1402c5d48837d1a77c87b98
AblaAM <ablaammami@think-it.io>
2021-10-05 19:28:48 +0100

    feat: Add `Modal` component

--- src/components/Model/index.tsx
+++ src/components/Model/index.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+import img from "../../assets/chuck-norris-does-not-approve.jpeg";
 
 const Model: React.FC = () => {
+  const [counter, setCounter] = useState<number>(25);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (counter > 0) {
+      setTimeout(() => setCounter(counter - 1), 1000);
+    } else {
+      setIsOpen(true);
+    }
+  }, [counter]);
+
   return (
     <>
-      <div style={{ height: window.innerHeight }}>hey</div>
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close" onClick={() => setIsOpen(false)}>
+              x
+            </button>
+
+            <img src={img} className="modal-image"></img>
+          </div>
+        </div>
+      )}
     </>
   );
 };
